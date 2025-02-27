0671df300d22216493ad58ceac58746877eb1897
Muhammad Mubashir <75811203+M-Mubashir674@users.noreply.github.com>
2025-02-27 17:41:23 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import "./styles/common.css";
-import AppRoutes from "./routes/AppRoutes";
+import AppRoutes from "./routes/AppRoutes.jsx";
 
 const App = () => <AppRoutes />;
 
